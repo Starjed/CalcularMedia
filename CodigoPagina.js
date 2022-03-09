473d03b3b91178b6a36d62454193ef8d8db53636
Starjed <biel123albuquerque@hotmail.com>
2022-03-09 11:38:48 -0400

    Alterações Feitas para melhorar o código

--- CodigoPagina.js
+++ CodigoPagina.js
@@ -4,11 +4,17 @@ function soma() {
     console.log(valor1)
     var valor2 = document.getElementById("valor2").value
     console.log(valor2)
-    var somaTotal = (parseInt(valor1) + parseInt(valor2)) / 2
+    var valor3 = document.getElementById("valor3").value
+    console.log(valor3)
+    var valor4 = document.getElementById("valor4").value
+    console.log(valor4)
+    var somaInicial =  (parseInt(valor1)) + (parseInt(valor2))
+    var somaInicial2 = (parseInt(valor3)) + (parseInt(valor4))
+    var somaTotal = (somaInicial + somaInicial2) / 4
 
-  
 
-    if (valor1 && valor2) {
+
+    if (valor1 && valor2 && valor3 && valor4) {
         document.getElementById("valorMedia").innerHTML = "A sua média foi: " + somaTotal
         console.log("Soma total " + somaTotal)
     }
